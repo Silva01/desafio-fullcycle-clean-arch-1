1818f2d3db8396ff30b386e6b480b28f9e8214ce
silva01 <silva20102@gmail.com>
2022-08-03 09:13:45 -0300

    Adicionado value objects de produto

--- src/main/kotlin/br/net/daniel/silva/desafio/domain/product/valueobject/valueObject.kt
+++ src/main/kotlin/br/net/daniel/silva/desafio/domain/product/valueobject/valueObject.kt
@@ -1,2 +1,14 @@
 package br.net.daniel.silva.desafio.domain.product.valueobject
 
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+interface ProductDTO {
+    val id: Int
+    val name: String
+    val description: String
+    val status: String
+    val createDate: LocalDateTime
+    val modifyDate: LocalDateTime
+    val price: BigDecimal
+}
