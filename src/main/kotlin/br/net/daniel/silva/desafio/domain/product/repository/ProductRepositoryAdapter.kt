e8af861732c6b4e8e8e7f28aea5020872bbe5d76
silva01 <silva20102@gmail.com>
2022-08-03 09:13:31 -0300

    Adicionado repository de produto

--- src/main/kotlin/br/net/daniel/silva/desafio/domain/product/repository/ProductRepositoryAdapter.kt
+++ src/main/kotlin/br/net/daniel/silva/desafio/domain/product/repository/ProductRepositoryAdapter.kt
@@ -1,4 +1,10 @@
 package br.net.daniel.silva.desafio.domain.product.repository
 
+import br.net.daniel.silva.desafio.domain.product.valueobject.ProductDTO
+
 interface ProductRepositoryAdapter {
+    fun create(productDTO: ProductDTO)
+    fun find(id: Int)
+    fun update(productDTO: ProductDTO)
+    fun list(): MutableList<ProductDTO>
 }
