b40eb268beddfb3fa97932f7ca9eba1e387f850a
silva01 <silva20102@gmail.com>
2022-08-05 09:40:25 -0300

    Adicionado entity de product para comunicação com a camada de banco de dados

---
+++ src/main/kotlin/br/net/daniel/silva/desafio/infrastructure/model/ProductEntity.kt
@@ -0,0 +1,35 @@
+package br.net.daniel.silva.desafio.infrastructure.model
+
+import java.math.BigDecimal
+import java.time.LocalDateTime
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "PRODUCT")
+open class ProductEntity {
+
+    @Id
+    @Column(name = "PRODUCT_ID")
+    open val id: Int? = null
+
+    @Column(name = "PRODUCT_NAME")
+    open val name: String? = null
+
+    @Column(name = "PRODUCT_DESCRIPTION")
+    open val description: String? = null
+
+    @Column(name = "PRODUCT_STATUS")
+    open val status: String? = null
+
+    @Column(name = "PRODUCT_CREATE_DATE")
+    open val createDate: LocalDateTime? = null
+
+    @Column(name = "PRODUCT_MODIFY_DATE")
+    open val modifyDate: LocalDateTime? = null
+
+    @Column(name = "PRODUCT_PRICE")
+    open val price: BigDecimal? = null
+}
