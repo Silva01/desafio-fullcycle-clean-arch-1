fb116d16371c72f5344f5a39a43ca13fda8c5ba3
silva01 <silva20102@gmail.com>
2022-08-05 09:27:06 -0300

    Adicionado dependencia do spring web

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework:spring-web")
+
 }
 
 tasks.withType<KotlinCompile> {
