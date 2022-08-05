2e37bf33d4b030dd1236440657af2891fafbfcff
silva01 <silva20102@gmail.com>
2022-08-05 09:42:18 -0300

    Adicionado regra de geração de ID

--- src/main/kotlin/br/net/daniel/silva/desafio/infrastructure/model/ProductEntity.kt
+++ src/main/kotlin/br/net/daniel/silva/desafio/infrastructure/model/ProductEntity.kt
@@ -2,10 +2,7 @@ package br.net.daniel.silva.desafio.infrastructure.model
 
 import java.math.BigDecimal
 import java.time.LocalDateTime
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.Table
+import javax.persistence.*
 
 @Entity
 @Table(name = "PRODUCT")
@@ -13,6 +10,7 @@ open class ProductEntity {
 
     @Id
     @Column(name = "PRODUCT_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     open val id: Int? = null
 
     @Column(name = "PRODUCT_NAME")
