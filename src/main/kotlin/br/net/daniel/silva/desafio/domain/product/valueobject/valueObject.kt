46ab4ecc41efd9dd6410f8b7bdcde63351609f83
silva01 <silva20102@gmail.com>
2022-08-09 09:39:26 -0300

    Modificando acesso dos atributos de input

--- src/main/kotlin/br/net/daniel/silva/desafio/domain/product/valueobject/valueObject.kt
+++ src/main/kotlin/br/net/daniel/silva/desafio/domain/product/valueobject/valueObject.kt
@@ -4,13 +4,13 @@ import java.math.BigDecimal
 import java.time.LocalDateTime
 
 interface ProductInputDTO {
-    val id: Int?
-    val name: String?
-    val description: String?
-    val status: String?
-    val createDate: LocalDateTime?
-    val modifyDate: LocalDateTime?
-    val price: BigDecimal?
+    var id: Int?
+    var name: String?
+    var description: String?
+    var status: String?
+    var createDate: LocalDateTime?
+    var modifyDate: LocalDateTime?
+    var price: BigDecimal?
 }
 
 interface ProductOutputDTO {
