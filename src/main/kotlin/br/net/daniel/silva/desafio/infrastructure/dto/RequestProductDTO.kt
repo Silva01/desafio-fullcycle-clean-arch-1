0797ffe29ca3ce4da1189b8cbeead19bef32eb27
silva01 <silva20102@gmail.com>
2022-08-09 09:40:22 -0300

    Modificando acesso dos atributos de input

--- src/main/kotlin/br/net/daniel/silva/desafio/infrastructure/dto/RequestProductDTO.kt
+++ src/main/kotlin/br/net/daniel/silva/desafio/infrastructure/dto/RequestProductDTO.kt
@@ -5,11 +5,11 @@ import java.math.BigDecimal
 import java.time.LocalDateTime
 
 class RequestProductDTO(
-    override val id: Int?,
-    override val name: String?,
-    override val description: String?,
-    override val status: String?,
-    override val createDate: LocalDateTime?,
-    override val modifyDate: LocalDateTime?,
-    override val price: BigDecimal?
+    override var id: Int?,
+    override var name: String?,
+    override var description: String?,
+    override var status: String?,
+    override var createDate: LocalDateTime?,
+    override var modifyDate: LocalDateTime?,
+    override var price: BigDecimal?
 ) : ProductInputDTO
