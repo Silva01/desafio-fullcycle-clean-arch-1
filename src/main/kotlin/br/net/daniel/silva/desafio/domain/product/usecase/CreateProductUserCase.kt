f52dfd0ac94dfec1a8c3e00504673ca504805dd4
silva01 <silva20102@gmail.com>
2022-08-09 09:40:45 -0300

    Adicionado preenchimento de createdDate

--- src/main/kotlin/br/net/daniel/silva/desafio/domain/product/usecase/CreateProductUserCase.kt
+++ src/main/kotlin/br/net/daniel/silva/desafio/domain/product/usecase/CreateProductUserCase.kt
@@ -2,7 +2,11 @@ package br.net.daniel.silva.desafio.domain.product.usecase
 
 import br.net.daniel.silva.desafio.domain.product.repository.ProductRepositoryAdapter
 import br.net.daniel.silva.desafio.domain.product.valueobject.ProductInputDTO
+import java.time.LocalDateTime
 
 internal class CreateProductUserCase(private val repository: ProductRepositoryAdapter) {
-    fun create(product: ProductInputDTO) = repository.create(product)
+    fun create(product: ProductInputDTO) {
+        product.createDate = LocalDateTime.now()
+        repository.create(product)
+    }
 }
