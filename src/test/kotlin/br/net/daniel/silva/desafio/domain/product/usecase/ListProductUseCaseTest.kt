7edfa517c9792b3ea4dbc7caa9e68f0c918b6277
silva01 <silva20102@gmail.com>
2022-08-11 09:01:56 -0300

    Implementado teste unitário de listar produto

--- src/test/kotlin/br/net/daniel/silva/desafio/domain/product/usecase/ListProductUseCaseTest.kt
+++ src/test/kotlin/br/net/daniel/silva/desafio/domain/product/usecase/ListProductUseCaseTest.kt
@@ -3,11 +3,14 @@ package br.net.daniel.silva.desafio.domain.product.usecase
 import br.net.daniel.silva.desafio.domain.product.repository.ProductRepositoryAdapter
 import br.net.daniel.silva.desafio.utils.generateListProductMock
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.*
 import org.springframework.boot.test.mock.mockito.MockBean
+import java.math.BigDecimal
 
 
+@Tag("unitTest")
 class ListProductUseCaseTest {
 
     @MockBean
@@ -21,6 +24,9 @@ class ListProductUseCaseTest {
 
         assertEquals(1, products.size)
         assertEquals(1, products[0].id)
-
+        assertEquals("mac", products[0].name)
+        assertEquals("produto de teste 1", products[0].description)
+        assertEquals("VENDIDO", products[0].status)
+        assertTrue(BigDecimal("100").equals(products[0].price))
     }
 }
