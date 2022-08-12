8038daf1ae5a5f807d4e0d6705c543a0fb07f8c8
silva01 <silva20102@gmail.com>
2022-08-12 08:33:45 -0300

    Ajustados mock

--- src/test/kotlin/br/net/daniel/silva/desafio/DesafioCleanArchFullCycleApplicationTests.kt
+++ src/test/kotlin/br/net/daniel/silva/desafio/DesafioCleanArchFullCycleApplicationTests.kt
@@ -1,9 +1,11 @@
 package br.net.daniel.silva.desafio
 
+import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
+@Tag("integration")
 class DesafioCleanArchFullCycleApplicationTests {
 
     @Test
--- src/test/kotlin/br/net/daniel/silva/desafio/utils/generateObjectTest.kt
+++ src/test/kotlin/br/net/daniel/silva/desafio/utils/generateObjectTest.kt
@@ -7,3 +7,4 @@ import java.time.LocalDateTime
 fun generateListProductMock(): MutableList<ProductOutputDTO> = mutableListOf(generateProductMock())
 
 fun generateProductMock(): ProductOutputDTO = ProductDTOTest(1, "mac", "produto de teste 1", "VENDIDO", LocalDateTime.now(), null, BigDecimal("100"))
+fun generateProducUpdatetMock(updateName: String): ProductOutputDTO = ProductDTOTest(1, updateName, "produto de teste 1", "VENDIDO", LocalDateTime.now(), null, BigDecimal("100"))
